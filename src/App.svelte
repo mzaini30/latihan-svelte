1587e89e2c64fbd236fda8f985bf69e43aa3e362
mzaini30 <muhzaini30@gmail.com>
2020-10-20 20:56:13 +0800

    upload

--- src/App.svelte
+++ src/App.svelte
@@ -2,15 +2,23 @@
 <hr>
 <p>Ini adalah list teman-temanku...</p>
 <ul>
-	<li>Satu</li>
-	<li>Dua</li>
-	<li>Tiga</li>
+	{#each list as x}
+		<li>{x}</li>
+	{/each}
 </ul>
-<form>
-	<input required type="" placeholder="Tambahkan teman" name="">
+<form on:submit|preventDefault={tambahkan}>
+	<input required type="" placeholder="Tambahkan teman" bind:value={baru} name="">
 </form>
 <style type="text/css">
 	.tengah {
 		text-align: center;
 	}
 </style>
+<script type="text/javascript">
+	let list = []
+	let baru = ''
+	const tambahkan = () => {
+		list = [baru, ...list]
+		baru = ''
+	}
+</script>
