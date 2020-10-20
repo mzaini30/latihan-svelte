009527f5768f1b83b573f9d1fb902864aee33d77
mzaini30 <muhzaini30@gmail.com>
2020-10-20 20:48:06 +0800

    upload

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<h3 class="tengah">List teman</h3>
+<hr>
+<p>Ini adalah list teman-temanku...</p>
+<ul>
+	<li>Satu</li>
+	<li>Dua</li>
+	<li>Tiga</li>
+</ul>
+<form>
+	<input required type="" placeholder="Tambahkan teman" name="">
+</form>
+<style type="text/css">
+	.tengah {
+		text-align: center;
+	}
+</style>
